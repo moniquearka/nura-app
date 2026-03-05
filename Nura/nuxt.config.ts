711f6bf82dad22712d9ed50cf4d1eac4dc6ff3e8
moniquearka <264465046+moniquearka@users.noreply.github.com>
2026-03-05 13:44:31 -0500

    fix: set /nura-app/ as default baseURL for GitHub Pages

--- Nura/nuxt.config.ts
+++ Nura/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const baseURL = process.env.NUXT_APP_BASE_URL || '/'
+const baseURL = process.env.NUXT_APP_BASE_URL || '/nura-app/'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
